26c80648a7de893fd98113b95196b452957ef80b
nousantx <nousantx@gmail.com>
2025-04-23 06:15:20 +0700

    feat(extractor): refactor class name extractor code

--- src/utils/classNameExtractor.js
+++ src/utils/classNameExtractor.js
@@ -5,31 +5,41 @@ export class Extractor {
   constructor(config = [], cssConfig = {}) {
     this.config = config
     this.cssConfig = cssConfig
-    this.regexp = new Moxie(cssConfig).regexp()
+    this.moxie = new Moxie(this.cssConfig)
   }
   extractClassNames(code) {
     try {
-      const cn = new Ngurai({
+      // regexp patterns from Moxie
+      const { all, prefix, type } = this.moxie.regexp()
+
+      // tokenize class names using NguraiJS
+      const tokenizer = new Ngurai({
         customOnly: true,
         noUnknownToken: true,
         noSpace: true,
         custom: {
           className: [
-            new RegExp(this.regexp.all), // catch all possible class names with value
-            new RegExp('(?:(' + this.regexp.prefix + '):)?' + this.regexp.type), // catch only valueless class name such as `flex` from `moxie.classes`
+            new RegExp(all), // catch all possible class names with value
+            new RegExp(`(?:(${prefix}):)?${type}`), // catch only valueless class name such as `flex` from `moxie.classes`
             ...this.config
           ]
         }
       })
 
-      const classNames = cn
-        .tokenize(code)
-        .flatMap((line) => line.filter((token) => token.type === 'className'))
-        .map((token) => token.value)
+      // get every className tokens from the tokenizer
+      const classNames = [
+        ...new Set(
+          tokenizer
+            .tokenize(code)
+            .flatMap(line => line.filter(token => token.type === 'className'))
+            .map(token => token.value)
+        )
+      ]
 
-      if (classNames.length === 0) return []
+      // validate the class name to ensure only valid class names processed
+      const validatedClassNames = this.moxie.process(classNames).map(item => item.raw[6])
 
-      return [...new Set(classNames)]
+      return validatedClassNames || []
     } catch (error) {
       console.error('Error extracting class names:', error)
       return []
